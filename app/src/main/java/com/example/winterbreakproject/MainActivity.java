d5bf4cb5df11684f5ad28e58efd63b4402e04dda
Princess <parkhayoung615@gmail.com>
2023-02-24 16:54:40 +0900

    Update Project

--- app/src/main/java/com/example/winterbreakproject/MainActivity.java
+++ app/src/main/java/com/example/winterbreakproject/MainActivity.java
@@ -14,21 +14,18 @@ public class MainActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
 
-    private GoogleMap mMap;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
-        mMap = googleMap;
 
         LatLng SEOUL = new LatLng(37.56, 126.97);
 
@@ -36,9 +33,9 @@ public class MainActivity extends AppCompatActivity
         markerOptions.position(SEOUL);
         markerOptions.title("서울");
         markerOptions.snippet("한국의 수도");
-        mMap.addMarker(markerOptions);
+        googleMap.addMarker(markerOptions);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 10));
 
     }
 
