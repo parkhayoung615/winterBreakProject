5e57dd85dff9811624ec0560f2e9157f64945282
jeong-a-young <monayoung0323@naver.com>
2023-02-26 15:45:47 +0900

    오늘의 팁 기능 구현 완료

--- app/src/main/java/com/example/winterbreakproject/activity/TodayTipActivity.java
+++ app/src/main/java/com/example/winterbreakproject/activity/TodayTipActivity.java
@@ -1,30 +1,102 @@
 package com.example.winterbreakproject.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.winterbreakproject.R;
-import com.example.winterbreakproject.request.TodayTipRequest;
+import com.example.winterbreakproject.dao.TodayTipDAO;
+import com.example.winterbreakproject.vo.TodayTipVO;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 public class TodayTipActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+    ArrayList<TodayTipVO> voArrayList = new ArrayList<>();
+
+    TodayTipDAO dao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // activity_main에 오늘의 팁 화면 넣기
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_today_tip);
 
-        String url = "http://monayoung0323.dothome.co.kr/TodayTip.php";
+        recyclerView=findViewById(R.id.recycler);
+        dao= new TodayTipDAO(this, voArrayList);
+        recyclerView.setAdapter(dao);
+
+        //리사이클러뷰의 레이아웃 매니저 설정
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        clickLoad();
+    }
+
+    public void clickLoad() {
+
+        //서버의 loadDBtoJson.php파일에 접속하여 (DB데이터들)결과 받기
+        //Volley+ 라이브러리 사용
+
+        //서버주소
+        String serverUrl="http://monayoung0323.dothome.co.kr/TodayTip.php";
+
+        //결과를 JsonArray 받을 것이므로..
+        //StringRequest가 아니라..
+        //JsonArrayRequest를 이용할 것임
+        JsonArrayRequest jsonArrayRequest= new JsonArrayRequest(Request.Method.POST, serverUrl, null, new Response.Listener<JSONArray>() {
+            //volley 라이브러리의 GET방식은 버튼 누를때마다 새로운 갱신 데이터를 불러들이지 않음. 그래서 POST 방식 사용
+            @Override
+            public void onResponse(JSONArray response) {
+                Toast.makeText(TodayTipActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+
+
+                //파라미터로 응답받은 결과 JsonArray를 분석
+
+                voArrayList.clear();
+                dao.notifyDataSetChanged();
+                try {
+
+                    for(int i=0;i<response.length();i++){
+                        JSONObject jsonObject= response.getJSONObject(i);
+
+                        int id= Integer.parseInt(jsonObject.getString("id")); //no가 문자열이라서 바꿔야함.
+                        String title=jsonObject.getString("title");
+                        String contents=jsonObject.getString("contents");
+
+                        voArrayList.add(0,new TodayTipVO(id, title, contents)); // 첫 번째 매개변수는 몇번째에 추가 될지, 제일 위에 오도록
+                        dao.notifyItemInserted(0);
+                    }
+                } catch (JSONException e) {e.printStackTrace();}
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(TodayTipActivity.this, "ERROR", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //실제 요청 작업을 수행해주는 요청큐 객체 생성
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        //요청큐에 요청 객체 생성
+        requestQueue.add(jsonArrayRequest);
+
+
     }
 }
