f6eef1671d6443a843c0ed586a5321468131c63b
Princess <parkhayoung615@gmail.com>
2023-02-27 00:59:36 +0900

    Update Project

--- app/src/main/java/com/example/winterbreakproject/MainActivity.java
+++ app/src/main/java/com/example/winterbreakproject/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.winterbreakproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+    private CustomDialog customDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //다이얼로그 밖의 화면은 흐리게 만들어줌
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount = 0.8f;
+        getWindow().setAttributes(layoutParams);
+    }
+
+
+    public void btnOnclick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                customDialog = new CustomDialog(this,"다이어로그에 들어갈 내용입니다.");
+                customDialog.show();
+                break;
+
+        }
     }
 }
