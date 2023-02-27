5e8a78da5d094ea3014b1c64d9aae1842a948446
xooslo <ys2110205@y-y.hs.kr>
2023-02-27 09:37:05 +0900

    update

--- app/src/main/java/com/example/winterbreakproject/MainActivity.java
+++ app/src/main/java/com/example/winterbreakproject/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.winterbreakproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    ImageView image_1;
+    public void btn_click(View view){
+        image_1 = findViewById(R.id.imageView);
+        if(view.getId() == R.id.closet_1){
+            image_1.setImageResource(R.drawable.basic);
+        } else if (view.getId() == R.id.closet_2) {
+            image_1.setImageResource(R.drawable.second);
+        }
+    }
 }
