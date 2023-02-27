a1e9f9d09eb0e731255ef6592d5638741c992f7f
jeong-a-young <monayoung0323@naver.com>
2023-02-27 16:12:54 +0900

    Update TodayTipRequest.java

--- app/src/main/java/com/example/winterbreakproject/request/TodayTipRequest.java
+++ app/src/main/java/com/example/winterbreakproject/request/TodayTipRequest.java
@@ -1,5 +1,27 @@
 package com.example.winterbreakproject.request;
 
-public class TodayTipRequest {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class TodayTipRequest extends StringRequest {
+    // 서버 URL 설정 (PHP 파일 연동)
+    final static private String URL = "http://monayoung0323.dothome.co.kr/TodayTipUpdate.php";
+    private Map<String, String> map;
+
+    public TodayTipRequest(String title, Response.Listener<String> listener) {
+        super(Request.Method.POST, URL, listener, null);
+
+        map = new HashMap<>();
+        map.put("title", title);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
 }
