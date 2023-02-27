1edc2f476230fbf38a85753483c7bc153bd22422
xooslo <ys2110205@y-y.hs.kr>
2023-02-27 22:06:36 +0900

    update
    버튼 radius 포기.. 이미지 쉐어 구현 전 커밋

--- app/src/main/java/com/example/winterbreakproject/MainActivity.java
+++ app/src/main/java/com/example/winterbreakproject/MainActivity.java
@@ -8,20 +8,9 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    ImageView image_1;
-    public void btn_click(View view){
-        image_1 = findViewById(R.id.imageView);
-        if(view.getId() == R.id.closet_1){
-            image_1.setImageResource(R.drawable.basic);
-        } else if (view.getId() == R.id.closet_2) {
-            image_1.setImageResource(R.drawable.second);
-        }
     }
 }
