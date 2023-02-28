6514a8a39b8ede3d3e369907454bfa018fc7ace6
Princess <parkhayoung615@gmail.com>
2023-02-28 17:50:21 +0900

    Update Project

--- app/src/main/java/com/example/winterbreakproject/activity/MainActivity.java
+++ app/src/main/java/com/example/winterbreakproject/activity/MainActivity.java
@@ -1,16 +1,15 @@
 package com.example.winterbreakproject.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.winterbreakproject.R;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
